0a1c8e2df770770f21e9cfe0a7b00d9b487065b3
solehudin5699 <solehudinamqi@yahoo.co.id>
2024-05-24 12:59:31 +0700

    update empty name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 //GET AVATAR
 app.get('/', async (req, res) => {
   const { name, size, background, color, bold, shape, rounded } = req.query;
-  const response = svgGenerator(`${name}`, {
+  const response = svgGenerator(name, {
     size,
     background,
     color,
